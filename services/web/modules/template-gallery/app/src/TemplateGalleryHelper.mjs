6b1b2df1a50995419c305503d8552918fdfc5431
FewPz <sharpkung4826@gmail.com>
2026-06-16 22:04:06 +0700

    fix: cannot compile

--- services/web/modules/template-gallery/app/src/TemplateGalleryHelper.mjs
+++ services/web/modules/template-gallery/app/src/TemplateGalleryHelper.mjs
@@ -8,7 +8,6 @@ import ProjectLocator from '../../../../app/src/Features/Project/ProjectLocator.
 import ProjectZipStreamManager from '../../../../app/src/Features/Downloads/ProjectZipStreamManager.mjs'
 import DocumentUpdaterHandler from '../../../../app/src/Features/DocumentUpdater/DocumentUpdaterHandler.mjs'
 import ClsiManager from '../../../../app/src/Features/Compile/ClsiManager.mjs'
-import CompileManager from '../../../../app/src/Features/Compile/CompileManager.mjs'
 import UserGetter from '../../../../app/src/Features/User/UserGetter.mjs'
 import { fetchStreamWithResponse } from '@overleaf/fetch-utils'
 import { Template } from './models/Template.mjs'
@@ -37,13 +36,18 @@ marked.use({
 
 function _createZipStreamForProjectAsync(projectId) {
   return new Promise((resolve, reject) => {
-    ProjectZipStreamManager.createZipStreamForProject(projectId, (err, archive) => {
-      if (err) {
-        return reject(err)
+    ProjectZipStreamManager.createZipStreamForProject(
+      projectId,
+      false,
+      undefined,
+      (err, archive) => {
+        if (err) {
+          return reject(err)
+        }
+        archive.on('error', (err) => reject(err))
+        resolve(archive)
       }
-      archive.on('error', (err) => reject(err))
-      resolve(archive)
-    })
+    )
   })
 }
 
@@ -139,11 +143,13 @@ export async function uploadTemplateAssets(projectId, userId, build, template) {
   try {
     [zipStream, pdfStream] = await Promise.all([
       _createZipStreamForProjectAsync(projectId),
-      CompileManager.promises
-        .getProjectCompileLimits(projectId)
-        .then((limits) =>
-          ClsiManager.promises.getOutputFileStream(projectId, userId, limits, undefined, build, 'output.pdf')
-        )
+      ClsiManager.promises.getOutputFileStream(
+        projectId,
+        userId,
+        undefined,
+        build,
+        'output.pdf'
+      ),
     ])
   } catch (error) {
     logger.error({ error, projectId }, 'No output.pdf?')
